bf74a1510266db09ccc95fc04b9c4e0e74487c8a
Nate River <85858293+nateriver19@users.noreply.github.com>
2021-07-16 10:12:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ export const App = () => {
   const [text, setText] = useState("");
   return (
     <div>
-      {/* <p data-testid="text-value"> {text} </p>
+      <p data-testid="text-value"> {text} </p>
 
       <input
         data-testid="input-text"
         placeholder="input value"
         value={text}
         onChange={(e) => setText(e.target.value)}
-      /> */}
+      />
     </div>
   );
 };
